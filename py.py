b9529565c3dedcdd05b12c9d3d7a55e0a01727ad
Stardrad <yin8086@gmail.com>
2024-09-12 13:14:46 +0800

    [feat] fix weekend.

--- py.py
+++ py.py
@@ -3,7 +3,7 @@ import requests
 import base64
 import random
 from datetime import datetime, time, timezone
-
+import pytz
 
 telegram_api_token = os.environ.get('TELEGRAM_API_TOKEN')
 telegram_chat_id = os.environ.get('TELEGRAM_CHAT_ID')
@@ -13,6 +13,20 @@ change_step2 = int(os.environ.get('CHANGE_STEP2'))
 change_step3 = int(os.environ.get('CHANGE_STEP3'))
 change_step4 = int(os.environ.get('CHANGE_STEP4'))
 target_step = int(os.environ.get('TARGET_STEP'))
+
+# 定义你想要的时区（比如，'Asia/Shanghai' 表示上海时间）
+timezone_name = 'Asia/Shanghai'
+timezone = pytz.timezone(timezone_name)
+
+# 获取当前时间并转换为指定时区的时间
+current_time_in_timezone = datetime.now(timezone)
+
+# 获取星期几（0表示星期一，6表示星期日）
+weekday_number = current_time_in_timezone.weekday()
+
+# 将数字转换为星期几的名称
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+weekday_name = weekdays[weekday_number]
 
 # 检查 Telegram 相关信息是否存在
 if telegram_api_token is None or telegram_chat_id is None:
@@ -94,6 +108,10 @@ else:
         else:
             min_steps = target_step - 16
             max_steps = target_step + 22
+
+        if (weekday_number == 5 or weekday_number == 6) :
+            min_steps = min_steps + 2000
+            max_steps = max_steps + 2000
 
         for account, password in account_password_pairs:
             result = modify_steps(account, password, min_steps, max_steps)
